94eeca057ccdbe815a32af11623c776efdb3d1e3
u-Kotovsky <kotovsky.2021@gmail.com>
2025-01-27 15:54:58 +0300

    Add project files.

---
+++ MDK_01.01_Lesson-10_Task1/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MDK_01._01_Lesson_10_Task1
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ MDK_01.01_Lesson-10_Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MDK_01._01_Lesson_10_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
